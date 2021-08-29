74f789601c0790e06e577aeb3f635d8d8b827326
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-29 19:42:15 +0900

    models.pyを定義。

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class FavoriteUrls(models.Model):
+    class Meta:
+        db_table = 'favorite_urls'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=30)
+    created_at = models.DateTimeField(verbose_name='登録日時', auto_now_add=True)
+
+    def __str__(self):
+        return self.title
