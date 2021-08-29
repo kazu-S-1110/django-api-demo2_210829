dcc3b8cc8f2cd04ce5d92a77fd19a9bdf85bc0c4
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-29 19:47:31 +0900

    adminを定義。

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import FavoriteUrls
 
-# Register your models here.
+class FavoModelAdmin(admin.ModelAdmin):
+    list_display = ("title","url" "id", "created_at")
+    ordering = ("-created_at",)
+
+
+admin.site.register(FavoriteUrls, FavoModelAdmin)
--- api/models.py
+++ api/models.py
@@ -8,6 +8,7 @@ class FavoriteUrls(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=30)
+    url = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(verbose_name='登録日時', auto_now_add=True)
 
     def __str__(self):
